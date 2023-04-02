d2d4144e8faff585f18185d42400bce3d3626703
jacekmalczyk <jacekmalczyk@jtendo.com>
2023-04-02 19:44:47 +0200

    fix(Form): Pending state is operational again.
    - Replace form with Form, so form is navigation aware.
    - Extract submit button to separate component.
    - Links replaced with nav button.
    - Extract logout button.

--- app/routes/entries.tsx
+++ app/routes/entries.tsx
@@ -1,6 +1,8 @@
-import { json, LoaderArgs, redirect } from "@remix-run/node";
+import type { LoaderArgs } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import { Outlet, useLoaderData } from "@remix-run/react";
 import { getUser } from "~/utils/session.server";
+import LogoutButton from "~/components/buttons/logout-button";
 
 export const loader = async ({ request }: LoaderArgs) => {
   const user = await getUser(request);
@@ -22,28 +24,7 @@ export default function EntriesRoute() {
           <h3 className={"flex-grow"}>
             Welcome, <b>{user?.username}</b>
           </h3>
-          <form action="/logout" method="post">
-            <button
-              type="submit"
-              className="inline-flex items-center  text-gray-400 hover:text-gray-300"
-            >
-              Logout
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="ml-1 h-5 w-5"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
-                />
-              </svg>
-            </button>
-          </form>
+          <LogoutButton />
         </div>
       )}
       <h1 className="text-5xl">Work Journal</h1>
