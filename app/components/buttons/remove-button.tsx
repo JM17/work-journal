099c2c6ef07fdae0344541aa3e87b7138384fd30
jacekmalczyk <jacekmalczyk@jtendo.com>
2023-04-11 22:43:42 +0200

    feat(RemoveButton): Add confirmation dialog

--- app/components/buttons/remove-button.tsx
+++ app/components/buttons/remove-button.tsx
@@ -1,16 +1,62 @@
 import { Form } from "@remix-run/react";
+import React, { useRef } from "react";
+import clsx from "clsx";
 
 export default function RemoveButton({ id }: { id: string }) {
+  const dialogRef = useRef<HTMLDialogElement>(null);
   return (
-    <Form method="post">
-      <input type="hidden" name="intent" value="remove" />
-      <input type="hidden" name="id" value={id} />
-      <button
-        type="submit"
-        className="inline-flex items-center  text-gray-400 hover:text-gray-300"
-      >
-        &times;
-      </button>
-    </Form>
+    <div>
+      <button onClick={() => dialogRef.current?.showModal()}>&times;</button>
+      <dialog id="dialog" ref={dialogRef}>
+        <div className="fixed inset-0 z-20 bg-black/50" />
+        <div
+          className={clsx(
+            "fixed z-50",
+            "w-[95vw] max-w-md rounded-lg p-4 md:w-full",
+            "left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%]",
+            "bg-white dark:bg-gray-800"
+          )}
+        >
+          <h2 className="text-sm font-medium text-gray-900 dark:text-gray-100">
+            Are you sure?
+          </h2>
+          <p className="mt-2 text-sm font-normal text-gray-700 dark:text-gray-400">
+            This action cannot be undone. This will permanently delete your
+            entry.
+          </p>
+          <div className={"mt-4 flex justify-end space-x-2"}>
+            <form method={"dialog"}>
+              <button
+                className={clsx(
+                  "inline-flex select-none justify-center rounded-md px-2 py-1 text-sm font-medium",
+                  "bg-white text-gray-900 hover:bg-gray-50 dark:bg-gray-700 dark:text-gray-100 hover:dark:bg-gray-600",
+                  "border border-gray-300 dark:border-transparent",
+                  "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
+                )}
+              >
+                Close
+              </button>
+            </form>
+
+            <Form method="delete">
+              <input type="hidden" name="intent" value="delete" />
+              <input type="hidden" name="id" value={id} />
+
+              <button
+                type="submit"
+                className={clsx(
+                  "inline-flex select-none justify-center rounded-md px-2 py-1 text-sm font-medium",
+                  "bg-red-600 text-white hover:bg-red-700 dark:bg-red-700 dark:text-gray-100 dark:hover:bg-red-600",
+                  "border border-transparent",
+                  "focus:outline-none focus-visible:ring focus-visible:ring-red-500 focus-visible:ring-opacity-75"
+                )}
+              >
+                Yes, delete entry
+              </button>
+            </Form>
+          </div>
+        </div>
+      </dialog>
+    </div>
   );
 }
